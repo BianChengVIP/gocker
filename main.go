89cfe1d1961c47fc162c753d9ef4ced0bd399532
U-HOME\Feng Yao Yun <568643727@qq.com>
2023-06-14 19:29:21 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+	"github.com/BianChengVIP/gocker/runc"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "gocker"
+	app.Usage = "gocker 是 golang 编写的精简版 Docker，目的是学习 Docker 的运行原理。"
+
+	app.Commands = []*cli.Command{
+		runc.InitCommand,
+		runc.RunCommand,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
